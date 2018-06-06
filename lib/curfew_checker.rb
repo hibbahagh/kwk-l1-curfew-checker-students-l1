5fc6abc7dfd9938cdf45f1edc8320912432e4d67
hibbahagh <hibbahagh@gmail.com>
2018-06-06 20:46:58 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,21 @@
 def simple_curfew_checker(time)
   # code goes here
+  if 11 <= 11 
+    puts "eleven is less than or equal to twe"
 end
 
-def curfew_checker(time)
-  # code goes here
-end
+# def curfew_checker(time)
+#   # code goes here
+# end
 
-def complex_curfew_checker(time)
-  # code goes here
-end
+# def complex_curfew_checker(time)
+#   # code goes here
+# end
 
-def deluxe_curfew_checker(time)
-  # code goes here
-end
+# def deluxe_curfew_checker(time)
+#   # code goes here
+# end
 
-def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
-end
+# def platinum_curfew_checker(current_time, curfew_time)
+#   # code goes here
+# end
